74917da91961e43a5e05bf17140a02edd39a758e
JadaC <158534853+JADA242@users.noreply.github.com>
2025-03-16 20:51:13 -0500

    Add files via upload

---
+++ include/env.h
@@ -0,0 +1,9 @@
+const char * SSID = "Wokwi-GUEST";
+const char * PASS = "";
+const int CHANNEL = 6;
+
+const bool IS_WOKWI = true;
+
+const char * ENDPOINT = "https://ecse3038-lab5-tester.onrender.com/message";
+const char * API_KEY = "Miriam#962";
+
